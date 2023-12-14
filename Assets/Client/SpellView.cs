edc8394980378c020f13e6f5833e5848065a5035
Alberto Alonso <alberto@alonso.xyz>
2023-12-15 01:39:00 +0200

    Interpolate the spell positions.

--- Assets/Client/SpellView.cs
+++ Assets/Client/SpellView.cs
@@ -5,14 +5,29 @@ public class SpellView : MonoBehaviour, ISyncEntity
     [SerializeField] private RectTransform rectTransform;
 
     public string spellName;
+    private bool hasData = false;
+    private float startPosition;
+    private float targetPosition;
+    private float startTime;
+    private float targetTime;
 
     string ISyncEntity.name => spellName;
 
     void ISyncEntity.ReceiveSync(int step, IState state_)
     {
         var state = state_ as SpellState;
-        float delta = 1f - (state.endStep - step) / (float)(state.endStep - state.startStep);
-        float pos = state.fromPlayer.position + (state.toPlayer.position - state.fromPlayer.position) * delta;
-        rectTransform.localPosition = new Vector3(pos, 0, 0);
+        startPosition = state.fromPlayer.position;
+        targetPosition = state.toPlayer.position;
+        startTime = state.startStep * Time.fixedDeltaTime;
+        targetTime = state.endStep * Time.fixedDeltaTime;
+        hasData = true;
+    }
+
+    void Update() {
+        if (hasData) {
+            float delta = (Time.time - startTime) / (targetTime - startTime);
+            float pos = startPosition + (targetPosition - startPosition) * delta;
+            rectTransform.localPosition = new Vector3(pos, 0, 0);
+        }
     }
 }
